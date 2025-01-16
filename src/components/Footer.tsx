c2ff64ba51aefade33dd92f06fd5cfaad01d96d4
James Drake <info@jamesmdrakemedia.com>
2025-01-16 09:42:40 -0500

    feat: styled footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
 export function Footer() {
+  const date = new Date();
+
   return (
     <>
-      <footer>&copy; 2025 James M. Drake, Jr. All rights reserved.</footer>
+      <footer>
+        <span>&copy; {date.getFullYear()} James M. Drake, Jr. All rights reserved.</span>
+        <a href="https://github.com/psilentrain1/jmdm-apps-application_tracker/issues" target="_blank">
+          Report an issue.
+        </a>
+      </footer>
     </>
   );
 }
