1ac207e6d60b2746f2e9835573bdd94362558cf8
James Drake <info@jamesmdrakemedia.com>
2025-01-18 14:05:47 -0500

    fix: fixed link highlighting for new app in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,30 @@
-import { HiOutlineBell, HiPlus, HiSearch } from "react-icons/hi";
+import { HiOutlineBell, HiPlus, HiSearch } from "react-icons/hi"
+import { Link } from "react-router"
 
 export function Header() {
-  return (
-    <>
-      <header>
-        <div className="search">
-          <form action="">
-            <input type="search" name="search" id="search" placeholder="Search..." />
-            <HiSearch />
-          </form>
-        </div>
-        <div className="buttons">
-          <button type="button">
-            <HiOutlineBell />
-          </button>
-          <a href="/edit/new">
-            <HiPlus />
-          </a>
-        </div>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <header>
+                <div className="search">
+                    <form action="">
+                        <input
+                            type="search"
+                            name="search"
+                            id="search"
+                            placeholder="Search..."
+                        />
+                        <HiSearch />
+                    </form>
+                </div>
+                <div className="buttons">
+                    <button type="button">
+                        <HiOutlineBell />
+                    </button>
+                    <Link to="/edit/new">
+                        <HiPlus />
+                    </Link>
+                </div>
+            </header>
+        </>
+    )
 }
