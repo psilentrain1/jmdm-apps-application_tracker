2795a5263371526a6c59eecf10c6c7803fd17b66
James Drake <info@jamesmdrakemedia.com>
2025-01-21 08:55:21 -0500

    feat: charts implemented

--- backend/services/applications.ts
+++ backend/services/applications.ts
@@ -99,40 +99,40 @@ function chart(chart: string) {
         case "appIndustry":
             query = `SELECT industry, COUNT(id) FROM applications GROUP BY industry;`
             break
-        case "appHistory":
-            query = `SELECT apply_date, COUNT(id) FROM applications GROUP BY apply_date;`
+        case "appTitle":
+            query = `SELECT title, COUNT(id) FROM applications GROUP BY title;`
             break
         case "appType":
             query = `SELECT type, COUNT(id) FROM applications GROUP BY type;`
             break
         case "intHistory":
-            query = `SELECT interview_date, COUNT(id) FROM applications GROUP BY interview_date;`
+            query = `;`
             break
         case "rejHistory":
-            query = `SELECT reject_date, COUNT(id) FROM applications GROUP BY reject_date;`
+            query = `;`
             break
         default:
-            query = `-- database: /Users/psilentrain1/Documents/development/jmdm-apps-application_tracker/backend/database/applications.db
+            query = `
 
-WITH combined_dates AS (
-    SELECT apply_date AS date FROM applications WHERE apply_date IS NOT NULL
-    UNION
-    SELECT interview_date AS date FROM applications WHERE interview_date IS NOT NULL
-    UNION
-    SELECT reject_date AS date FROM applications WHERE reject_date IS NOT NULL
-)
-SELECT
-    cd.date AS dates,
-    COUNT(t1.apply_date) AS num_applications,
-    COUNT(t1.interview_date) AS num_interviews,
-    COUNT(t1.reject_date) AS num_rejections
-FROM combined_dates cd
-LEFT JOIN applications t1 ON cd.date = t1.apply_date
-    OR cd.date = t1.interview_date
-    OR cd.date = t1.reject_date
-GROUP BY cd.date
-ORDER BY cd.date;
-`
+            WITH combined_dates AS (
+                SELECT apply_date AS date FROM applications WHERE apply_date IS NOT NULL
+                UNION
+                SELECT interview_date AS date FROM applications WHERE interview_date IS NOT NULL
+                UNION
+                SELECT reject_date AS date FROM applications WHERE reject_date IS NOT NULL
+            )
+            SELECT
+                cd.date AS dates,
+                COUNT(t1.apply_date) AS num_applications,
+                COUNT(t1.interview_date) AS num_interviews,
+                COUNT(t1.reject_date) AS num_rejections
+            FROM combined_dates cd
+            LEFT JOIN applications t1 ON cd.date = t1.apply_date
+                OR cd.date = t1.interview_date
+                OR cd.date = t1.reject_date
+            GROUP BY cd.date
+            ORDER BY cd.date;
+            `
     }
 
     const data = db.query(query)
--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -9,27 +9,27 @@ export function Charts() {
         series: [
             { type: "pie", angleKey: "COUNT(id)", sectorLabelKey: "industry" },
         ],
+        title: {
+            text: "Applications by Industry",
+        },
     })
 
-    const [chart_AppSubmissionHistory, setChart_AppSubmissionHistory] =
-        useState({
-            data: [],
-            series: [{ type: "line" }],
-        })
+    const [chart_AppSubsByTitle, setChart_AppSubsByTitle] = useState({
+        data: [],
+        series: [
+            { type: "pie", angleKey: "COUNT(id)", sectorLabelKey: "title" },
+        ],
+        title: {
+            text: "Applications by Job Title",
+        },
+    })
 
     const [chart_AppSubsByType, setChart_AppSubsByType] = useState({
         data: [],
         series: [{ type: "bar", xKey: "type", yKey: "COUNT(id)" }],
-    })
-
-    const [chart_InterviewHistory, setChart_InterviewHistory] = useState({
-        data: [],
-        series: [{ type: "line" }],
-    })
-
-    const [chart_AppsVRejections, setChart_AppsVRejections] = useState({
-        data: [],
-        series: [{ type: "line" }],
+        title: {
+            text: "Applications by Job Type",
+        },
     })
 
     const [chart_History, setChart_History] = useState({
@@ -40,20 +40,37 @@ export function Charts() {
                 xKey: "dates",
                 yKey: "num_applications",
                 yName: "Applications",
+                stroke: "orange",
+                marker: {
+                    fill: "orange",
+                },
             },
             {
                 type: "line",
                 xKey: "dates",
                 yKey: "num_interviews",
                 yName: "Interviews",
+                stroke: "green",
+                marker: {
+                    fill: "green",
+                    shape: "square",
+                },
             },
             {
                 type: "line",
                 xKey: "dates",
                 yKey: "num_rejections",
                 yName: "Rejections",
+                stroke: "red",
+                marker: {
+                    fill: "red",
+                    shape: "triangle",
+                },
             },
         ],
+        title: {
+            text: "Application History",
+        },
     })
 
     useEffect(() => {
@@ -63,19 +80,21 @@ export function Charts() {
         axios.get(SERVER_URL + "chart/appType").then(({ data }) => {
             setChart_AppSubsByType({ ...chart_AppSubsByType, data: data })
         })
+        axios.get(SERVER_URL + "chart/appTitle").then(({ data }) => {
+            setChart_AppSubsByTitle({ ...chart_AppSubsByTitle, data: data })
+        })
         axios.get(SERVER_URL + "chart/big").then(({ data }) => {
+            data.pop()
             setChart_History({ ...chart_History, data: data })
         })
     }, [])
 
     return (
         <>
-            <AgCharts options={chart_AppByIndustry} />
-            {/* <AgCharts options={chart_AppSubmissionHistory} /> */}
-            <AgCharts options={chart_AppSubsByType} />
-            {/* <AgCharts options={chart_InterviewHistory} /> */}
-            {/* <AgCharts options={chart_AppsVRejections} /> */}
             <AgCharts options={chart_History} />
+            <AgCharts options={chart_AppSubsByTitle} />
+            <AgCharts options={chart_AppByIndustry} />
+            <AgCharts options={chart_AppSubsByType} />
         </>
     )
 }
